4dd11e7c8b59f8a217690be16705be51fc59588f
Helga Velroyen <helgav@google.com>
2014-02-28 10:02:13 +0100

    adapt makecerts_openssl to work again

--- makecerts_openssl.sh
+++ makecerts_openssl.sh
@@ -3,8 +3,10 @@
 SERIAL_FILE=serial
 INDEX_FILE=index.txt
 
-CA_CERT=cacert.pem
-CA_KEY=cakey.pem
+CA_CERT=ca_cert.pem
+echo "CA_CERT ${CA_CERT}"
+CA_KEY=ca_key.pem
+echo "CA_KEY ${CA_KEY}"
 
 SERVER_REQ=server_req.pem
 echo "SERVER_REQ ${SERVER_REQ}"
@@ -22,21 +24,14 @@ echo "CLIENT_CERT ${CLIENT_CERT}"
 
 CONFIG_FILE=$(pwd)/openssl.cnf
 echo "CONFIG_FILE ${CONFIG_FILE}"
-CERT_DIR=$(pwd)/certs
-echo "CERT_DIR ${CERT_DIR}"
-PRIVATE_DIR=$(pwd)/private
-echo "PRIVATE_DIR ${PRIVATE_DIR}"
 
 rm ${SERIAL_FILE}
 echo '01' > ${SERIAL_FILE}
 rm ${INDEX_FILE}
 touch ${INDEX_FILE}
-rm ${CERT_DIR}/*
-mkdir -p ${CERT_DIR}
-mkdir -p ${PRIVATE_DIR}
 
 # create root cert:
-OPENSSL_CONF=${CONFIG_FILE} openssl req -x509 -newkey rsa:2048 -out ${CA_CERT} -outform PEM -key ${PRIVATE_DIR}/${CA_KEY} -nodes
+OPENSSL_CONF=${CONFIG_FILE} openssl req -x509 -newkey rsa:2048 -out ${CA_CERT} -outform PEM -nodes
 
 # check what it looks like:
 OPENSSL_CONF=${CONFIG_FILE} openssl x509 -in ${CA_CERT} -text -noout
